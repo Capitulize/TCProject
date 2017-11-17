6cc8b2e9b6852e5dc12b3429c92ee1dd51f3c2c9
SZ-Vapor <33229415+SZ-Vapor@users.noreply.github.com>
2017-11-17 16:22:17 -0500

    Update FileServer.java

--- FileServer.java
+++ FileServer.java
@@ -1,2 +1,9 @@
+import java.io.*;
+import java.net.*;
 public class FileServer{
+  static OutputStream outPut;
+  public static void main (String args[]){
+  ServerSocket serSock = new ServerSocket(45000);
+  }
+  
 }
