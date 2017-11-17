ac1b5fbe141b33407f90e890c0a8427d3c7c2594
Capitulize <33671168+Capitulize@users.noreply.github.com>
2017-11-17 16:46:08 -0500

    Update FileClient.java

--- FileClient.java
+++ FileClient.java
@@ -1,5 +1,20 @@
-public class FileClient{
+import java.net.*;
+import java.io.*;
 
+public class FileClient{
 public static void main(String args[]){
+  
+  //Initialize socket
+        Socket socket = new Socket(InetAddress.getByName("localhost"), 50005);
+        byte[] contents = new byte[10000];
+  
+ //Initialize the FileOutputStream to the output file's full path.
+        FileOutputStream fos1 = new FileOutputStream("Howdy2.txt");
+        FileOutputStream fos2 = new FileOutputStream("Crowdy2.txt");
+        FileOutputStream fos3 = new FileOutputStream("Lmao2.txt");
+        BufferedOutputStream bos1 = new BufferedOutputStream(fos1);
+        BufferedOutputStream bos2 = new BufferedOutputStream(fos2);
+        BufferedOutputStream bos3 = new BufferedOutputStream(fos3);
+        InputStream is = socket.getInputStream();
   }
 }
