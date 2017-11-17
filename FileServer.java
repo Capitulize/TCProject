b1983d5bd0ff013139f3b1b77d1b9e98fc427fc6
SZ-Vapor <33229415+SZ-Vapor@users.noreply.github.com>
2017-11-17 16:39:54 -0500

    Update FileServer.java

--- FileServer.java
+++ FileServer.java
@@ -1,9 +1,22 @@
+/**
+* Sends files to client
+*/
 import java.io.*;
 import java.net.*;
 public class FileServer{
   static OutputStream outPut;
   public static void main (String args[]){
-  ServerSocket serSock = new ServerSocket(45000);
+    ServerSocket serSock = new ServerSocket(45000);
+    Socket sock = serSock.accept();//returns socket used for specific slient
+    
+    InetAddress adr = InetAddress.getByName("localhost");//change name to whatever is needed
+    
+  }
+  
+  private void byteStream(File file) throws IOException{
+    int fileLength = file.length();
+    FileInputStream input1 = new FileInputStream(file);
+    //BufferedInputStream
   }
   
 }
