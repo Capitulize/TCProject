43a53de9cc59227e7ee684e2f79115ddae04b146
Steven Hogenson <33229415+SZ-Vapor@users.noreply.github.com>
2017-11-20 00:54:14 -0500

    Update FileClient.java

--- FileClient.java
+++ FileClient.java
@@ -8,13 +8,20 @@ public static void main(String args[]){
         Socket socket = new Socket(InetAddress.getByName("localhost"), 50005);
         byte[] contents = new byte[10000];
   
- //Initialize the FileOutputStream to the output file's full path.
-        FileOutputStream fos1 = new FileOutputStream("Howdy2.txt");
-        FileOutputStream fos2 = new FileOutputStream("Crowdy2.txt");
-        FileOutputStream fos3 = new FileOutputStream("Lmao2.txt");
-        BufferedOutputStream bos1 = new BufferedOutputStream(fos1);
-        BufferedOutputStream bos2 = new BufferedOutputStream(fos2);
-        BufferedOutputStream bos3 = new BufferedOutputStream(fos3);
+  //Send choice to FileServer
+  BufferedReader inFromUser = new BufferedReader(new InputStreamRead(System.in));
+  DataOutputStrem outToServer = new DataOutputStream(socket.getOutputStream());
+  String choice = inFromUser.readLine();
+  outToServer.writeBytes(choice+'\n');
+  
+ //Initialize in  choice "if" statements as to prevent empty files from being created
+        FileOutputStream fos1; 
+        FileOutputStream fos2; 
+        FileOutputStream fos3; 
+        BufferedOutputStream bos1; 
+        BufferedOutputStream bos2;
+        BufferedOutputStream bos3;
+  
         InputStream is = socket.getInputStream();
   }
 }
