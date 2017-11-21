f0b62d29007b9bff2f598ae5848f98f4000666c6
Steven Hogenson <33229415+SZ-Vapor@users.noreply.github.com>
2017-11-20 22:41:59 -0500

    Update FileClient.java

--- FileClient.java
+++ FileClient.java
@@ -5,9 +5,9 @@ public class FileClient{
 public static void main(String args[]){
   
   //Initialize socket
-        Socket socket = new Socket(InetAddress.getByName("localhost"), 50005);
-        byte[] contents = new byte[10000];
-  
+        Socket socket = new Socket(InetAddress.getByName("localhost"), 55555);
+        byte[] fileBytes = new byte[10000];
+  InputStream is = socket.getInputStream();
   //Send choice to FileServer
   BufferedReader inFromUser = new BufferedReader(new InputStreamRead(System.in));
   DataOutputStrem outToServer = new DataOutputStream(socket.getOutputStream());
@@ -15,13 +15,32 @@ public static void main(String args[]){
   outToServer.writeBytes(choice+'\n');
   
  //Initialize in  choice "if" statements as to prevent empty files from being created
-        FileOutputStream fos1; 
-        FileOutputStream fos2; 
-        FileOutputStream fos3; 
-        BufferedOutputStream bos1; 
-        BufferedOutputStream bos2;
-        BufferedOutputStream bos3;
+        FileOutputStream fileOut;//creates new file
+        if(choice.equals(""))
+          System.exit(0);
+        if(choice.equals("1")){
+          fileOut = new FileOutputStream("C:\\...nameOfNewFile1.txt");
+          BufferedOutputStream buffOut = new BufferedOutputStream(fileOut);
+          
+          //buffOut.write(fileBytes);
+          buffOut.flush();
+        }
+        if(choice.equals("2")){
+          fileOut = new FileOutputStream("C:\\...nameOfNewFile2.txt");
+          BufferedOutputStream buffOut = new BufferedOutputStream(fileOut);
+          
+          //buffOut.write(fileBytes);
+          buffOut.flush();
+        }
+        if(choice.equals("3")){
+          fileOut = new FileOutputStream("C:\\...nameOfNewFile3.txt");
+          BufferedOutputStream buffOut = new BufferedOutputStream(fileOut);
+          
+          //buffOut.write(fileBytes);
+          buffOut.flush();
+        }
+      
   
-        InputStream is = socket.getInputStream();
+        
   }
 }
