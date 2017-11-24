3a4474fdebddba1cbfc8f041e65609ce80ab6956
Capitulize <33671168+Capitulize@users.noreply.github.com>
2017-11-24 06:02:46 -0500

    Update FileServer.java
    Fixed a bunch of typos again, and made the program connect, send the file, and properly print it with the other pull request.

--- FileServer.java
+++ FileServer.java
@@ -5,15 +5,15 @@ import java.io.*;
 import java.net.*;
 public class FileServer{
   static OutputStream outPut;
-  public static void main (String args[]){
+  public static void main (String args[]) throws IOException{
     ServerSocket serSock = new ServerSocket(45000);
-    Socket sock = serSock.accept();//returns socket used for specific slient
+    Socket sock = serSock.accept();//returns socket used for specific client
     
     File file1 = new File("aye.txt");//change to whatever
     File file2 = new File("cray.txt");
     File file3 = new File("day.txt");
     //Add additional files here
-    sendFileNames();//sends a list of available files to the client upon connection
+    sendFileNames(sock, file1, file2, file3);//sends a list of available files to the client upon connection
     String choice;
     outPut = sock.getOutputStream();
     BufferedReader inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -31,7 +31,7 @@ public class FileServer{
     * Add however many other files to send here
     */
     else
-      invalidIn();//Sends invalid input message to client upon receiving a request for a nonexistent file
+      invalidIn(sock);//Sends invalid input message to client upon receiving a request for a nonexistent file
     
     sock.close();
     serSock.close();
@@ -40,25 +40,40 @@ public class FileServer{
     
   }
   
-  private void byteStream(File file) throws IOException{
-    long fileLength = file.length();
-    FileInputStream fInput = new FileInputStream(file);
-    BufferedInputStream buffIn = new BufferedInputStream(fInput);
-
-    int fLength = (int)fileLength;//had to typecast to put into byte array
-    byte[] fileByte= new byte[fLength];
-    buffIn.read(fileByte,0,fLength);
-    outPut.write(fileByte);
-    
+  private static void byteStream(File file) throws IOException{
+	  long fileLength = file.length();
+      byte[] contents;
+      long current = 0;
+      FileInputStream input1 = new FileInputStream(file);
+      BufferedInputStream bis = new BufferedInputStream(input1);
+      
+      while(current!=fileLength){
+          int size = 1000000000;
+          if(fileLength - current >= size){
+              current += size;
+          
+          }
+          else{
+              size = (int)(fileLength - current);
+              
+              current = fileLength;
+              
+          }
+          contents = new byte[size];
+          bis.read(contents, 0, size);
+          outPut.write(contents);
+          System.out.println("Sending file ... "+(current*100)/fileLength+"% complete!");
+          bis.close();
+      }
     
   }
-  private static void sendFileNames() throws IOException{
-    DataOutputStream outToCLient = newDataOutputStream(socket.getOutputStream());
-    String fileList = "Available Files:  (1)"+file1.getName;//add for however many files there are
+  private static void sendFileNames(Socket sock, File file1, File file2, File file3) throws IOException{
+    DataOutputStream outToClient = new DataOutputStream(sock.getOutputStream());
+    String fileList = "Available Files:  (3) " + file1.getName() + " " + file2.getName() + " " + file3.getName();//add for however many files there are
     outToClient.writeBytes(fileList+"\n");
   }
-  private static void invalidIn() throws IOException{
-    DataOutputStream outToCLient = newDataOutputStream(socket.getOutputStream());
+  private static void invalidIn(Socket sock) throws IOException{
+    DataOutputStream outToClient = new DataOutputStream(sock.getOutputStream());
     String invalid = "Invalid input. Please select another file to receive.";
     outToClient.writeBytes(invalid+"\n"); 
   }
