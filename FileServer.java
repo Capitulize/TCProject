b2a33aa844facd8dde4016a272b77c221d77197a
Capitulize <33671168+Capitulize@users.noreply.github.com>
2017-11-26 02:35:26 -0500

    Update FileServer.java
    Some big changes again, overall structure and file sending changed. 
    Big changes include: able to accept more than 1 file, 2 sockets to correlate with fileClient, splitting off tasks into methods, writing large files perfectly. With the current updates to the project, it fulfills all the requirements needed.

--- FileServer.java
+++ FileServer.java
@@ -1,81 +1,80 @@
-/**
-* Sends files to client
-*/
 import java.io.*;
 import java.net.*;
-public class FileServer{
-  static OutputStream outPut;
-  public static void main (String args[]) throws IOException{
-    ServerSocket serSock = new ServerSocket(45000);
-    Socket sock = serSock.accept();//returns socket used for specific client
-    
-    File file1 = new File("aye.txt");//change to whatever
-    File file2 = new File("cray.txt");
-    File file3 = new File("day.txt");
-    //Add additional files here
-    sendFileNames(sock, file1, file2, file3);//sends a list of available files to the client upon connection
-    String choice;
-    outPut = sock.getOutputStream();
-    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-    choice = inFromClient.readLine();//lets client choose what file to receive
-    
-    if(choice.equals(""))
-      System.exit(0);
-   else if(choice.equals("1"))
-       byteStream(file1);
-    else if (choice.equals("2"))
-      byteStream(file2);
-    else if (choice.equals("3"))
-      byteStream(file3);
-    /*
-    * Add however many other files to send here
-    */
-    else
-      invalidIn(sock);//Sends invalid input message to client upon receiving a request for a nonexistent file
-    
-    sock.close();
-    serSock.close();
-                                                     
-    
-    
-  }
-  
-  private static void byteStream(File file) throws IOException{
-	  long fileLength = file.length();
-      byte[] contents;
-      long current = 0;
-      FileInputStream input1 = new FileInputStream(file);
-      BufferedInputStream bis = new BufferedInputStream(input1);
-      
-      while(current!=fileLength){
-          int size = 1000000000;
-          if(fileLength - current >= size){
-              current += size;
-          
-          }
-          else{
-              size = (int)(fileLength - current);
-              
-              current = fileLength;
-              
-          }
-          contents = new byte[size];
-          bis.read(contents, 0, size);
-          outPut.write(contents);
-          System.out.println("Sending file ... "+(current*100)/fileLength+"% complete!");
-          bis.close();
-      }
-    
-  }
-  private static void sendFileNames(Socket sock, File file1, File file2, File file3) throws IOException{
-    DataOutputStream outToClient = new DataOutputStream(sock.getOutputStream());
-    String fileList = "Available Files:  (3) " + file1.getName() + " " + file2.getName() + " " + file3.getName();//add for however many files there are
-    outToClient.writeBytes(fileList+"\n");
-  }
-  private static void invalidIn(Socket sock) throws IOException{
-    DataOutputStream outToClient = new DataOutputStream(sock.getOutputStream());
-    String invalid = "Invalid input. Please select another file to receive.";
-    outToClient.writeBytes(invalid+"\n"); 
-  }
-  
+
+public class FileServer {
+
+    static OutputStream os;
+    public static int fLength;
+    static Socket socket;
+    static Socket socket2;
+    static File file1 = new File("XD.txt");
+    static File file2 = new File("Pride and Prejudice.txt");
+    static File file3 = new File("Lmao.txt");
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Running...");
+        ServerSocket ssock2 = new ServerSocket(50016);
+        socket2 = ssock2.accept();
+        sendFileNames();
+        while (true) {
+            ServerSocket ssock = new ServerSocket(50015);
+            socket = ssock.accept();           
+            
+           
+            os = socket.getOutputStream();
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String choice = inFromClient.readLine();
+           
+            if (choice.equals("")) {
+
+                System.exit(0);
+            } else if (choice.equals("1")) {
+                byteStream(file1);
+                System.out.println("File sent successfully!");
+            } else if (choice.equals("2")) {
+                byteStream(file2);
+                System.out.println("File sent successfully!");
+            } else if (choice.equals("3")) {
+                byteStream(file3);
+                System.out.println("File sent successfully!");
+            } else {
+                invalidIn();
+                //System.out.println("Invalid Input.");
+            }
+
+            os.flush();
+           
+            socket.close();
+            ssock.close();
+            
+        } 
+    }
+
+    private static void byteStream(File file) throws IOException {
+       
+        long fileLength = file.length();
+        FileInputStream fInput = new FileInputStream(file);
+        BufferedInputStream buffIn = new BufferedInputStream(fInput);
+
+        fLength = (int) fileLength;//had to typecast to put into byte array
+        byte[] fileByte = new byte[fLength];
+        buffIn.read(fileByte, 0, fLength);
+        os.write(fileByte);
+    }
+
+    private static void sendFileNames() throws IOException {
+        DataOutputStream outToClient = new DataOutputStream(socket2.getOutputStream());
+        String sentence = "Available Files:  (1):" + file1.getName() + "   (2):" + file2.getName() + "   (3):" + file3.getName();
+        outToClient.writeBytes(sentence + "\n");
+
+    }
+
+    private static void invalidIn() throws IOException {
+        DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+        String sentence = "Invalid Input";
+        outToClient.writeBytes(sentence + "\n");
+
+    }
+
 }
